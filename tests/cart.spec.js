9333034172877aa0e26c6369d2716da92056e045
pottee <pottee@hotmail.com>
2024-08-16 21:12:17 +0700

    apply test.step instead, and readable in BDD style.

--- tests/cart.spec.js
+++ tests/cart.spec.js
@@ -1,24 +1,32 @@
 const { test } = require('../pages/base.page');
 const testData = require('../test-data/productItemsData.json');
 
-test('Guest user adds a product to cart successfully', async ({ homePage, searchPage, itemDetailsPage, topMenu, cartPage, page }) => {
+test('As guest user, I can add a product at item details page to cart', async({ homePage, searchPage, itemDetailsPage, topMenu, cartPage }) => {
 
-    await homePage.openPage();
-    await homePage.acceptPrivacy();
+    await test.step('When I select a product item from search result', async () => {
 
-    await searchPage.searchForKeyword(testData.searchKeyword);
-    await searchPage.productItemsLoaded();
-    await searchPage.selectProduct(testData.productItem1.itemName); 
-    
-    await itemDetailsPage.addToCartButtonLoaded();
-    await itemDetailsPage.clickAddToCart();
+        await homePage.openPage();
+        await homePage.acceptPrivacy();
 
-    await topMenu.assertNumberOfItems(1);
-    
-    await itemDetailsPage.viewToCartButtonLoaded();
-    await itemDetailsPage.clickViewToCart();
-    
-    await cartPage.buyNowButtonLoaded();
-    await cartPage.assertProductAdded(testData.productItem1);
-    
+        await searchPage.searchForKeyword(testData.searchKeyword);
+        await searchPage.productItemsLoaded();
+        await searchPage.selectProduct(testData.productItem1.itemName); 
+    });
+    await test.step('And I add the product to cart', async () => {
+
+        await itemDetailsPage.addToCartButtonLoaded();
+        await itemDetailsPage.clickAddToCart();
+    });
+    await test.step('Then I should see number 1 in cart icon', async () => {
+
+        await topMenu.assertNumberOfItems(1);
+    });
+    await test.step('And I should see the product information in my cart', async () => {
+        
+        await itemDetailsPage.viewToCartButtonLoaded(); // it might navigate to cart page by clicking cart badge icon first then 'View cart' button instead. If it's not in item details page and the item already added.
+        await itemDetailsPage.clickViewToCart();
+        
+        await cartPage.buyNowButtonLoaded();
+        await cartPage.assertProductAdded(testData.productItem1);
+    });
 });
